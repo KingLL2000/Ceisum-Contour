e5c65ecaf1dac41b2966284525ee904d56b17644
王华波 <253320465@qq.com>
2026-02-11 16:39:14 +0800

    add example

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/Cesium-Contour/index.js'),
       name: 'CesiumContour',
-      fileName: 'cesium-contour',
-      formats: ['es', 'umd']
+      formats: ['es', 'iife']
     },
     rollupOptions: {
       // 移除 external，将所有依赖打包在一起
       external: ['cesium'],
+      output: {
+        // 为不同格式指定不同的全局变量名
+        globals: {
+          cesium: 'Cesium'
+        }
+      }
     },
     copyPublicDir: false
   },
